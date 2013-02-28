dbc64808c5d9a802274883f5ff5ba7ef39c977ee
Michael <michael@thinkcreative.com>
2013-02-28 12:07:20 -0500

    create repository

---
+++ ThinkCreativeDebugBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ThinkCreative\DebugBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ThinkCreativeDebugBundle extends Bundle
+{
+
+	protected $name = "ThinkCreativeDebugBundle";
+
+}
